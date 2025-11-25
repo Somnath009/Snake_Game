fd1c686fc488420fcad5594f6b2666f64c5a040a
Somnath Paul <08paulsomnath@gmail.com>
2025-11-25 16:29:59 +0530

    Add some more functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const board = document.querySelector(".board");
+const startButton = document.querySelector(".btn-start");
+const modal = document.querySelector(".modal")
+
 const blockHeight = 80;
 const blockWidth = 80;
 
@@ -17,10 +20,16 @@ const snake = [
     y: 5,
   },
 ];
+
 let direction = "down"
+let intervalId = null
+
+
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+let food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
@@ -29,20 +38,16 @@ for (let row = 0; row < rows; row++) {
     block.classList.add("block");
     board.appendChild(block);
 
-    block.innerHTML = `${row}-${col}`
+    // block.innerHTML = `${row}-${col}`
 
     blocks[`${row}-${col}`] = block;
   }
 }
 
 function render(){
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.add("fill")
-  })
-}
-
-setInterval(() => {
   let head = null;
+
+  blocks[`${food.x}-${food.y}`].classList.add("food")
 
   if(direction == "left"){
     head = { x: snake[0].x, y: snake[0].y - 1}
@@ -54,16 +59,43 @@ setInterval(() => {
     head = { x: snake[0].x - 1, y: snake[0].y}
   }
 
+  if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
+    alert("Game Over");
+    clearInterval(intervalId);
+  }
+
+  if(head.x == food.x && head.y == food.y){
+    blocks[`${food.x}-${food.y}`].classList.remove("food")
+    food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)}
+    blocks[`${food.x}-${food.y}`].classList.add("food")
+
+    snake.unshift(head)
+  }
+
   snake.forEach((segment) => {
     blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
   })
 
-  // snake.unshift(head)
-  // snake.pop()
+  snake.unshift(head)
+  snake.pop()
 
 
-  render();
-}, 300);
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+  })
+}
+
+// intervalId = setInterval(() => {
+  
+//   render();
+// }, 300);
+
+startButton.addEventListener("click", () => {
+  modal.style.display = "none"
+
+  intervalId = setInterval(() => { render() }, 300);
+})
 
 addEventListener("keydown", (dets) => {
   if(dets.key == "ArrowUp"){
